cf7d481e1e2b67751cc7738b94da2594aedb6546
Mikel Fröse <mfroese@uandi.com>
2013-05-07 14:55:42 +0200

    Integrated multidomain alternative paths to the smarty view

--- classes/SmartyView.php
+++ classes/SmartyView.php
@@ -9,4 +9,38 @@ class SmartyView extends Kohana_SmartyView {
 
 		return $_instance;
 	}
+
+
+	public function set_filename($file) {
+		if (($path = Kohana::find_file('views', $file, Kohana::$config->load('smarty')->get('tpl_extension'))) === FALSE) {
+			// check the alternative paths
+			$path = $this->checkAlternativeFilePaths($file);
+		}
+
+		// Store the file path locally
+		$this->_file = $path;
+
+		return $this;
+	}
+
+	protected function checkAlternativeFilePaths($file) {
+		$origFile = $file;
+		$multidomainPaths = Helper_Config::getSmartyIncludePaths();
+
+		$request = Request::current();
+		$file = trim(str_replace($request->domainTemplatepath(), '', $file), DIRECTORY_SEPARATOR);
+
+		foreach ($multidomainPaths as $domainPath) {
+			$tmpFile = trim($domainPath,DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $file;
+
+			if (($path = Kohana::find_file('views', $tmpFile, Kohana::$config->load('smarty')->get('tpl_extension'))) !== FALSE) {
+				return $path;
+			}
+		}
+
+		// File not found, now throw the exception
+		throw new View_Exception('The requested view :file could not be found', array(
+			':file' => $origFile,
+		));
+	}
 }
